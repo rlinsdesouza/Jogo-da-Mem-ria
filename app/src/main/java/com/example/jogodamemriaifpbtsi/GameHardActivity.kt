ca6dc49b7bf8dbcb1601a3de5bcb780798478954
Rafael Lins de Souza <20171370003@ad.ifpb.edu.br>
2019-05-17 18:59:38 -0300

    gridview

--- app/src/main/java/com/example/jogodamemriaifpbtsi/GameHardActivity.kt
+++ app/src/main/java/com/example/jogodamemriaifpbtsi/GameHardActivity.kt
@@ -3,28 +3,16 @@ package com.example.jogodamemriaifpbtsi
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.Toast
+import android.util.Log
+import android.widget.*
+import com.example.jogodamemriaifpbtsi.DAO.DAO
+import com.example.jogodamemriaifpbtsi.DAO.ServerCallback
 import com.squareup.picasso.Picasso
+import org.json.JSONObject
 
 class GameHardActivity : AppCompatActivity() {
-    private lateinit var imagem1 : ImageView
-    private lateinit var imagem2 : ImageView
-    private lateinit var imagem3 : ImageView
-    private lateinit var imagem4 : ImageView
-    private lateinit var imagem5 : ImageView
-    private lateinit var imagem6 : ImageView
-    private lateinit var imagem7 : ImageView
-    private lateinit var imagem8 : ImageView
-    private lateinit var imagem9 : ImageView
-    private lateinit var imagem10 : ImageView
-    private lateinit var imagem11: ImageView
-    private lateinit var imagem12: ImageView
-    private lateinit var imagem13 : ImageView
-    private lateinit var imagem14 : ImageView
-    private lateinit var imagem15 : ImageView
-    private lateinit var imagem16 : ImageView
-
+    private lateinit var ListImageView : MutableList<ImageView>
+    private lateinit var gridview : GridView
     private lateinit var baseImagens: List<Object>
     private lateinit var ImagensEmJogo: List<String>
 
@@ -34,21 +22,46 @@ class GameHardActivity : AppCompatActivity() {
 
     private lateinit var pontos : Integer
 
-
-
+    private lateinit var professores : MutableList<JSONObject>
+    private lateinit var dao: DAO
+    private var qntCartas : Int = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_hard)
-
-        this.imagem1 = findViewById(R.id.ImagensIV11)
-        this.imagem1.setOnClickListener({
-            Picasso.get().load("http://res.cloudinary.com/deqmrmqui/image/upload/v1557696533/kpry3ib3h3tu06dxbqdf.jpg").into(this.imagem1)
+        this.dao = DAO(this)
+        dao.getProfessores(object : ServerCallback {
+            override fun onSucess(result: MutableList<JSONObject>) {
+               professores = result
+               Log.e("APP_TESTE",professores.toString())
+            }
         })
+//        Log.e("APP_TESTE",professores.toString())
 
+    }
 
+    override fun onResume() {
+        super.onResume()
+        var imageView : ImageView = ImageView(this)
+        var adapter: ArrayAdapter<ImageView>
 
+        this.gridview = findViewById(R.id.GameGVid)
+        for(i in 0..16) {
+            imageView.setImageResource(R.drawable.duvida)
+            imageView.setTag(i)
+            this.ListImageView.add(imageView)
+        }
+        adapter = ArrayAdapter(this,R.id.GameGVid,this.ListImageView)
+        this.gridview.addView()
+//        for(professor:JSONObject in professores) {
+//            imageView = ImageView(this)
+//            imageView.setImageResource(R.drawable.duvida)
+//            imageView.setTag(professor.getString("foto"))
+//        }
+//            this.imagem1.setOnClickListener({
+//            Picasso.get().load("http://res.cloudinary.com/deqmrmqui/image/upload/v1557696533/kpry3ib3h3tu06dxbqdf.jpg").into(this.imagem1)
+//        })
     }
 
     fun verificaClick () {
